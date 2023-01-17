43ed19f57be6cb869c7d2a749eace955e0dbfa18
Shakira Perry <shaaperr@ut.utm.edu>
2023-01-17 13:58:40 -0600

    test push

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿/* Shakira Perry 
+ * 01/17/2023
+ * Classwork 1
+ */
 
-namespace HelloWorld
+using System;
+
+namespace Class_work_1
 {
+
     class Program
     {
+        private int steve;
+        public void setSteve(int s)
+        {
+            steve = s;
+        }
+
+        public int getSteve()
+        {
+            return steve;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadKey();
+            Console.WriteLine("Setting Steve to 4");
+            Program p = new Program();
+            p.setSteve(4);
+            Console.WriteLine("Steve is: " + p.getSteve());
+            Console.ReadKey();
         }
     }
 }
