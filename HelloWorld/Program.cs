179fc89e08bc0238daabf305b3f7e1d7f2784394
Shakira Perry <shaaperr@ut.utm.edu>
2023-01-22 15:30:08 -0600

    Proper guess from 0 - 100 and code cleaned up

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,25 +10,14 @@ namespace Class_work_1
 
     class Program
     {
-      /*  private int steve;
-        public void setSteve(int s)
-        {
-            steve = s;
-        }
-
-        public int getSteve()
-        {
-            return steve;
-        }*/
         static void Main(string[] args)
         {
             
             var rand = new Random();
             Console.WriteLine("I am thinking of a number between 0 and 100.");
-            //Console.ReadKey();
-            //Console.WriteLine(rand.Next(101));
-            int randN = rand.Next(50, 101);
-            //Console.WriteLine(randN);
+           
+            int randN = rand.Next(101);
+            
             Console.WriteLine("What number am I thinking of? ");
             int userIn = Convert.ToInt32(Console.ReadLine());
             int count = 0;
@@ -52,17 +41,15 @@ namespace Class_work_1
             Console.WriteLine("\n Correct guess! It took you " + count + " tries.");
 
             
-            // Console.WriteLine();
-            //Console.Write("{0,8:N0}", rand.Next(50, 101));
-            //Program p = new Program();
-            //p.setSteve(4);
-            //Console.WriteLine("Steve is: " + p.getSteve());
-            //Console.ReadKey();
-            // Console.WriteLine("Range X?");
-            // int userInX = Convert.ToInt32(Console.ReadLine());
+            
+/*
+            Console.WriteLine("Range X?");
+            int userInX = Convert.ToInt32(Console.ReadLine());
 
-            //Console.WriteLine("Limit Y?");
-            //int userInY = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Limit Y?");
+            int userInY = Convert.ToInt32(Console.ReadLine());
+
+            int randN = rand.Next(50, 101); */
 
         }
     }
