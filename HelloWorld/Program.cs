f933e979aa09aeaf57696104b49d01251ea7ec21
Shakira Perry <shaaperr@ut.utm.edu>
2023-01-22 15:38:54 -0600

    Range User Guess Properly Done in branch feature

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -13,43 +13,42 @@ namespace Class_work_1
         static void Main(string[] args)
         {
             
+
             var rand = new Random();
-            Console.WriteLine("I am thinking of a number between 0 and 100.");
-           
-            int randN = rand.Next(101);
-            
+
+            Console.WriteLine("Range X (minimum)?");
+            int userInX = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Limit Y? (maximum)");
+            int userInY = Convert.ToInt32(Console.ReadLine());
+
+            int randN = rand.Next(userInX, userInY);
+
+            Console.WriteLine("I am thinking of a number between X and Y.");
+
+
             Console.WriteLine("What number am I thinking of? ");
             int userIn = Convert.ToInt32(Console.ReadLine());
             int count = 0;
-            while (userIn != randN){
-                
-                    if (userIn > randN)
-                    {
-                        Console.WriteLine("You are above the number I'm thinking of!");
-                        Console.WriteLine("What number am I thinking of? ");
-                        userIn = Convert.ToInt32(Console.ReadLine());
-                    }
-                    else if (userIn < randN)
-                    {
-                        Console.WriteLine("You are below the number I'm thinking of!");
-                        Console.WriteLine("What number am I thinking of? ");
-                        userIn = Convert.ToInt32(Console.ReadLine());
-                    }
-                    
-                count++;
+            while (userIn != randN)
+            {
+
+                if (userIn > randN)
+                {
+                    Console.WriteLine("You are above the number I'm thinking of!");
+                    Console.WriteLine("What number am I thinking of? ");
+                    userIn = Convert.ToInt32(Console.ReadLine());
                 }
+                else if (userIn < randN)
+                {
+                    Console.WriteLine("You are below the number I'm thinking of!");
+                    Console.WriteLine("What number am I thinking of? ");
+                    userIn = Convert.ToInt32(Console.ReadLine());
+                }
+
+                count++;
+            }
             Console.WriteLine("\n Correct guess! It took you " + count + " tries.");
-
-            
-            
-/*
-            Console.WriteLine("Range X?");
-            int userInX = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Limit Y?");
-            int userInY = Convert.ToInt32(Console.ReadLine());
-
-            int randN = rand.Next(50, 101); */
 
         }
     }
